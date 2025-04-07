7a415a3f46c3b4e64ff35b9f7def2dc1ad9e054f
harun habib <zulbijadinhabibharun3755@gmail.com>
2025-04-08 02:30:45 +0300

    B searching

---
+++ binary search.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// Iterative Binary Search Function
+int binarySearchIterative(int arr[], int size, int target) {
+    int left = 0, right = size - 1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        // Check if the target is present at mid
+        if (arr[mid] == target) {
+            return mid;
+        }
+        // If target is greater, ignore the left half
+        if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        // If target is smaller, ignore the right half
+        else {
+            right = mid - 1;
+        }
+    }
+    
+    // If target is not found
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9, 11, 13};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+    
+    int result = binarySearchIterative(arr, size, target);
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+    
+    return 0;
+}
+

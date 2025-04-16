8b0f1f3ad2f108568eab387d76580f6b2f1203be
harun habib <zulbijadinhabibharun3755@gmail.com>
2025-04-16 08:46:34 +0300

    second or third largest number

--- the second or the third largest number.cpp
+++ the second or the third largest number.cpp
@@ -2,12 +2,10 @@
 #include <iostream>
 #include <climits>
 using namespace std;
-
-// Function to find the second largest element
 int findSecondLargest(int arr[], int size) {
     if (size < 2) {
         cout << "Array should have at least two elements!" << endl;
-        return INT_MIN;  // return minimum integer value to indicate an error
+        return INT_MIN;  
     }
     
     int first = INT_MIN, second = INT_MIN;
@@ -22,13 +20,12 @@ int findSecondLargest(int arr[], int size) {
     }
     
     return second;
-}
+} 
 
-// Function to find the third largest element
 int findThirdLargest(int arr[], int size) {
     if (size < 3) {
         cout << "Array should have at least three elements!" << endl;
-        return INT_MIN;  // return minimum integer value to indicate an error
+        return INT_MIN; 
     }
     
     int first = INT_MIN, second = INT_MIN, third = INT_MIN;
@@ -48,18 +45,15 @@ int findThirdLargest(int arr[], int size) {
     
     return third;
 }
-
 int main() {
     int arr[] = {34, 15, 88, 2, 5, 29, 89};
     int size = sizeof(arr) / sizeof(arr[0]);
-    
-    // Find and print the second largest element
+
     int secondLargest = findSecondLargest(arr, size);
     if (secondLargest != INT_MIN) {
         cout << "The second largest number in the array is: " << secondLargest << endl;
     }
     
-    // Find and print the third largest element
     int thirdLargest = findThirdLargest(arr, size);
     if (thirdLargest != INT_MIN) {
         cout << "The third largest number in the array is: " << thirdLargest << endl;
